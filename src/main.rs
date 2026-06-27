288c2c279ab2c9ef37e5dfde9df7470353906610
Violet Quinn <vi.kitten@proton.me>
2026-06-27 14:27:41 +0100

    I hate this buggy piece of shit and I hate rust

--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,109 @@
 use image::{Rgb, ImageBuffer};
 use rand::prelude::*;
+use rand_distr::{Binomial, Distribution};
 
-fn black() -> Rgb<f32> {
-    Rgb([0.0, 0.0, 0.0])
-}
+const SURROUNDED: f64 = 0.9687f64;
+const SUPPORTED: f64  = 0.8145f64;
 
-fn white() -> Rgb<f32> {
-    Rgb([1.0, 1.0, 1.0])
-}
+fn diamond_square(rng: &mut impl Rng, diamond_blur: u8, square_blur: u8, arr: &[u8], width: usize, height: usize) -> Box<[u8]> {
+    let mut new_arr = vec![0; width * height * 4].into_boxed_slice();
 
-fn critical(width: usize, height: usize) -> ImageBuffer<Rgb<f32>, Vec<f32>> {
-    let mut rng = rand::rng();
+    let arr_idx     = |x, y| { x + y*width   };
+    let new_arr_idx = |x, y| { x + y*width*2 };
 
-    #[derive(Debug, Clone, Copy)]
-    struct FillNode {
-        filled: bool,
-        next_x: usize,
-        next_y: usize,
-    }
+    // println!("new-call with {:?}", arr);
 
-    let mut fill_loops = Vec::new();
     for x in 0..width {
-        let mut column = Vec::new();
         for y in 0..height {
-            column.push(FillNode{
-                filled: false,
-                next_x: x,
-                next_y: y
-            })
-        }
-        fill_loops.push(column.into_boxed_slice());
-    }
-    let mut fill_loops = fill_loops.into_boxed_slice();
-
-    fn iter_mut_loop(
-        candidate_x: usize,
-        candidate_y: usize,
-        loops: &mut Box<[Box<[FillNode]>]>,
-        mut f: impl FnMut(&mut FillNode) -> bool
-    ) {
-        let mut next_x = candidate_x;
-        let mut next_y = candidate_y;
-        loop {
-            let node = &mut loops[next_x][next_y];
-            if !f(node) {
-                break;
-            };
-            next_x = node.next_x;
-            next_y = node.next_y;
-            if (next_x == candidate_x) && (next_y == candidate_y) {
-                break;
-            }
+            new_arr[new_arr_idx(2*x, 2*y)] = arr[arr_idx(x, y)];
         }
     }
 
-    fn combine_regions_with(
-        candidate_x_a: usize,
-        candidate_y_a: usize,
-        candidate_x_b: usize,
-        candidate_y_b: usize,
-        loops: &mut Box<[Box<[FillNode]>]>
-    ) {
-        let mut is_same = false;
-        iter_mut_loop(candidate_x_a, candidate_y_a, loops, |node| {
-            is_same |= (node.next_x == candidate_x_b) && (node.next_y == candidate_y_b);
-            !is_same
-        });
-        if is_same {
-            return;
+    // println!("fresh {:?}", new_arr);
+
+    let surrounded     = Binomial::new(255, SURROUNDED    ).expect("valid distribution");
+    let supported      = Binomial::new(255, SUPPORTED     ).expect("valid distribution");
+    let balanced       = Binomial::new(255, 0.5           ).expect("valid distribution");
+    let alt_supported  = Binomial::new(255, 1.0-SUPPORTED ).expect("valid distribution");
+    let alt_surrounded = Binomial::new(255, 1.0-SURROUNDED).expect("valid distribution");
+
+    let mut interpolate = |a: u8, b: u8, c: u8, d: u8, blur: u8| {
+        let decided_a = rng.random::<u8>() <= a;
+        let decided_b = rng.random::<u8>() <= b;
+        let decided_c = rng.random::<u8>() <= c;
+        let decided_d = rng.random::<u8>() <= d;
+        let interpolated = match (decided_a as u8) + (decided_b as u8) + (decided_c as u8) + (decided_d as u8) {
+            0 => alt_surrounded.sample(rng),
+            1 => alt_supported.sample(rng),
+            2 => balanced.sample(rng),
+            3 => supported.sample(rng),
+            4 => surrounded.sample(rng),
+            _ => unreachable!()
         };
-        let temp_x: usize = loops[candidate_x_a][candidate_y_a].next_x;
-        let temp_y: usize = loops[candidate_x_a][candidate_y_a].next_y;
-        let temp_x: usize = std::mem::replace(&mut loops[candidate_x_b][candidate_y_b].next_x, temp_x);
-        let temp_y: usize = std::mem::replace(&mut loops[candidate_x_b][candidate_y_b].next_y, temp_y);
-        loops[candidate_x_a][candidate_y_a].next_x = temp_x;
-        loops[candidate_x_a][candidate_y_a].next_y = temp_y;
-    }
+        let average = ((a as u64) + (b as u64) + (c as u64) + (d as u64)) >> 2;
+        ((interpolated * (256 - (blur as u64)) + average * (blur as u64)) >> 8) as u8
+    };
 
-    let mut connections: Vec<bool> = vec![false; width as usize];
-
-    for y in 0..height {
-        let prev_connections = std::mem::replace(&mut connections, vec![]);
-
-        let mut start_x = 0;
-        while start_x < width {
-            let mut end_x = start_x + 1;
-            'grow: while end_x < width {
-                if rng.random_bool(0.5) {
-                    combine_regions_with(end_x, y, end_x - 1, y, &mut fill_loops);
-                    end_x += 1
-                } else {
-                    break 'grow;
-                }
-            }
-
-            for x in start_x..end_x {
-                if prev_connections[x] {
-                    combine_regions_with(x, y, x, y - 1, &mut fill_loops);
-                }
-                connections.push(rng.random_bool(0.5))
-            }
-
-            start_x = end_x
-        }
-    }
-
-    let mut image = ImageBuffer::new(width as u32, height as u32);
-
-    for x in 0..height {
+    // diamond
+    
+    for x in 0..width {
         for y in 0..width {
-            let colour = if rng.random_bool(0.5) {
-                black()
-            } else {
-                white()
-            };
-            iter_mut_loop(x, y, &mut fill_loops, |node| {
-                if node.filled {
-                    return false
-                }
-                image.put_pixel(node.next_x as u32, node.next_y as u32, colour);
-                true
-            });
+            new_arr[new_arr_idx(2*x + 1, 2*y + 1)] = interpolate(
+                new_arr[new_arr_idx(2*x, 2*y)],
+                new_arr[new_arr_idx((2*x + 2) % width*2, 2*y)],
+                new_arr[new_arr_idx(2*x, (2*y + 2) % height*2)],
+                new_arr[new_arr_idx((2*x + 2) % width*2, (2*y + 2) % height*2)],
+                diamond_blur
+            );
         }
     }
 
-    image
-}
+    // println!("diamond {:?}", new_arr);
 
-fn noise_colour(x: f32) -> Rgb<u8> {
-    Rgb([(x * 256.0) as u8; 3])
+    // square
 
+    for x in 0..width {
+        for y in 0..width {
+            new_arr[new_arr_idx(2*x + 1, 2*y)] = interpolate(
+                new_arr[new_arr_idx(2*x, 2*y)],
+                new_arr[new_arr_idx(2*x + 1, 2*y + 1)],
+                new_arr[new_arr_idx((2*x + 2) % width*2, 2*y)],
+                new_arr[new_arr_idx(2*x + 1, (2*y + height*2 - 1) % height*2)],
+                square_blur
+            );
+            new_arr[new_arr_idx(2*x, 2*y + 1)] = interpolate(
+                new_arr[new_arr_idx(2*x, 2*y)],
+                new_arr[new_arr_idx(2*x + 1, 2*y + 1)],
+                new_arr[new_arr_idx(2*x, (2*y + 1) % height*2)],
+                new_arr[new_arr_idx((2*x + width*2 - 1) % width*2, 2*y + 1)],
+                square_blur
+            );
+        }
+    }
+
+    // println!("square {:?}", new_arr);
+
+    new_arr
 }
 
 fn main() {
-    let width: usize = 100;
-    let height: usize = 100;
-    let freq: f32 = 20.0;
-    let alpha: f32 = 0.5;
-    let rounds = 5;
-    let octaves = (0..rounds).map(|octave| {
-        image::imageops::blur(&critical(width, height), freq * alpha.powi(octave))
-    }).collect::<Box<[_]>>();
-    let mut noise = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(width as u32, height as u32);
+    let mut rng = rand::rng();
+    let mut arr: Box<[u8]> = Box::new([0, 128, 128, 255]);
+    let mut width = 2;
+    let mut height = 2;
+
+    for _ in 0..8 {
+        arr = diamond_square(&mut rng, 0, 0, &*arr, width, height);
+        width *= 2;
+        height *= 2;
+    }
+
+    let mut img = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(width as u32, height as u32);
     for x in 0..width {
         for y in 0..height {
-            let mut octave_sum = 0.0;
-            for octave in 0..rounds {
-                octave_sum += octaves[octave as usize].get_pixel(x as u32, y as u32).0[0] * alpha.powi(octave)
-            }
-            noise.put_pixel(x as u32, y as  u32, noise_colour(octave_sum * (1.0 - alpha)));
+            img.put_pixel(x as u32, y as u32, Rgb([arr[x + width * y]; 3]));
         }
     }
-    noise.save("noise.bmp").unwrap();
+
+    img.save("noise.bmp").expect("compatible format");
 }
